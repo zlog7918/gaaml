775f07838c9313aec3618f64bf252ec959ddd0a2
Zlog <zlog@protonmail.com>
2025-11-27 23:39:14 +0100

    rewritten scope of project

--- proj/src/app.py
+++ proj/src/app.py
@@ -1,4 +1,15 @@
 import typing as t
+# import keras as krs
+# import tensorflow
 
 def cr_app() -> None:
+  # seq=krs.models.Sequential()
+  # seq.compile(
+  #   krs.optimizers.Adam(learning_rate=0.001), # type: ignore[arg-type]
+  #   loss=None
+  # )
+  # seq.fit(
+  #   epochs=1
+  # )
+
   pass
