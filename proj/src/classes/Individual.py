1162000ff56a415295394b20549f5d4bb30b6071
Zlog <zlog@protonmail.com>
2025-12-09 13:24:18 +0100

    individual as interface

--- proj/src/classes/Individual.py
+++ proj/src/classes/Individual.py
@@ -1,9 +1,10 @@
-from __future__ import annotations
+import abc
 import typing as t
 
 _T=t.TypeVar('_T')
-class Individual(t.Generic[_T]):
+class Individual(t.Generic[_T], metaclass=abc.ABCMeta):
   gen: _T
   def __init__(self, gen: _T):
     self.gen=gen
+  @abc.abstractmethod
   def mutate(self) -> None: ...
