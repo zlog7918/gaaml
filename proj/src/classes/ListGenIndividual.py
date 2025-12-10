56ecbd0447116d6595fa94e9a99b534e186c68b4
Zlog <zlog@protonmail.com>
2025-12-10 20:18:05 +0100

    change of item size var

--- proj/src/classes/ListGenIndividual.py
+++ proj/src/classes/ListGenIndividual.py
@@ -5,24 +5,24 @@ from . import utils as util
 from .Individual import Individual
 
 class ListGenIndividual(Individual[str]):
-  gen_size: int
+  item_size: int
   @t.overload
-  def __init__(self, num_gens: int, gen_size: int, /): ...
+  def __init__(self, num_gens: int, item_size: int, /): ...
   @t.overload
   def __init__(self, a: ListGenIndividual, b: ListGenIndividual, /, cross_point: tuple[int, int]): ...
   def __init__(self, a: int|ListGenIndividual, b: int|ListGenIndividual, /, cross_point: tuple[int, int]|None=None):
     if isinstance(a, int) and isinstance(b, int):
       super().__init__(util.int_to_bin(rnd.getrandbits(a), a))
-      self.gen_size=b
+      self.item_size=b
       return
     if isinstance(a, int) or isinstance(b, int) or cross_point is None:
       raise Exception('Illegal argument options')
-    if a.gen_size!=b.gen_size:
+    if a.item_size!=b.item_size:
       raise Exception('First and second solution do not have equal gen size')
     if any(cp<0 or cp>len(gen) for gen, cp in zip((a.gen, b.gen), cross_point)):
       raise Exception('Cross point is out side of solution')
     super().__init__(a.gen[:cross_point[0]]+b.gen[cross_point[1]:])
-    self.gen_size=a.gen_size
+    self.item_size=a.item_size
 
   def mutate(self) -> None:
     i=rnd.randint(0, len(self.gen)-1)
@@ -33,13 +33,13 @@ class ListGenIndividual(Individual[str]):
   _VGI=t.TypeVar('_VGI', bound=ListGenIndividual)
   @classmethod
   def get_cp(cls: type[_VGI], a: _VGI, b: _VGI) -> tuple[int, int]:
-    if a.gen_size!=b.gen_size:
+    if a.item_size!=b.item_size:
       raise Exception('First and second solution do not have equal gen size')
     ia=rnd.randint(0, len(a.gen))
-    r=ia%a.gen_size
-    lb=len(b.gen)//b.gen_size
+    r=ia%a.item_size
+    lb=len(b.gen)//b.item_size
     ib=rnd.randint(0, lb if r==0 else lb-1)
-    ib=ib*b.gen_size+r
+    ib=ib*b.item_size+r
     return ia, ib
 
   @classmethod
