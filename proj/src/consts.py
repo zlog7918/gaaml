7ae3f6abc094c3dbad40e5f7595263d54ba1a582
Zlog <zlog@protonmail.com>
2025-12-11 17:52:51 +0100

    change of learning_rate const

--- proj/src/consts.py
+++ proj/src/consts.py
@@ -9,9 +9,9 @@ BIN_PART_REST=(
   ('neuron_num_seed', (10, 0, 1023)),
   ('neuron_type_seed', (10, 0, 1023)),
   ('optimizer', (1, 0, 1)),
-  ('learning_rate', (13, 1, 8192)),
-  # TODO: maybe this version [2^(-13) - 1.] → [2^(-13) - 0.5]
-  # ('learning_rate', (12, 1, 4096)),
+  ('learning_rate', (12, 1, 4096)),
+  # TODO: maybe this version [2^(-13) - 0.5] → [2^(-13) - 1.]
+  # ('learning_rate', (13, 1, 8192)),
   ('epochs', (10, 1, 1024)),
 )
 
