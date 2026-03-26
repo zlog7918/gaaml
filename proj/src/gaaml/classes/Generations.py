23c26822e11b2fa32456a177e713e58f2ada338a
Zlog <zlog@protonmail.com>
2026-03-26 13:01:18 +0100

    modified Generations class to fit tests

--- proj/src/gaaml/classes/Generations.py
+++ proj/src/gaaml/classes/Generations.py
@@ -3,42 +3,56 @@ from . import _utils as util
 from .Population import Population
 
 class Generations(t.Generic[util.IndividualType]):
-  max_sol: util.IndividualType|None=None
-  min_sol: util.IndividualType|None=None
-  max_of_max=-1.
-  min_of_min=float('inf')
-  def __init__(self, pop: Population[util.IndividualType], num_gen: int) -> None:
+  __max_sol: util.IndividualType|None=None
+  __min_sol: util.IndividualType|None=None
+  __max_of_max=-1.
+  __min_of_min=float('inf')
+  __curr_generations=0
+  def __init__(self, pop: Population[util.IndividualType], max_num_gen: int) -> None:
     super().__init__()
     # assert num_gen>0
+    if max_num_gen<1:
+      raise ValueError('max_num_gen: is too small, it should at least equal 1')
+    self.__pop=pop
+    self.__max_num_gen=max_num_gen
+    self.__maxs: list[float]=[.0]*(self.__max_num_gen+1)
+    self.__avgs: list[float]=[.0]*(self.__max_num_gen+1)
+    self.__mins: list[float]=[.0]*(self.__max_num_gen+1)
+    self.__arrange_min_max(self.__curr_generations)
+
+  def go_through_generations(self, num_gen: int|None=None) -> None:
+    if num_gen is None:
+      num_gen=self.__max_num_gen-self.__curr_generations
+    elif num_gen>self.__max_num_gen-self.__curr_generations:
+      num_gen=self.__max_num_gen-self.__curr_generations
     if num_gen<1:
-      raise ValueError('num_gen: is too small, it should at least equal 1')
-    self.pop=pop
-    self.num_gen=num_gen
-    self.maxs: list[float]=[0]*(self.num_gen+1)
-    self.avgs: list[float]=[0]*(self.num_gen+1)
-    self.mins: list[float]=[0]*(self.num_gen+1)
+      raise IndexError('Tried to add next generation(s) after reaching max number of them')
+    for _ in range(num_gen):
+      self.__pop.next_generation()
+      self.__curr_generations+=1
+      self.__arrange_min_max(self.__curr_generations)
 
-  def go_through_generations(self) -> tuple[tuple[util.IndividualType, util.IndividualType], tuple[float, float], tuple[list[float], list[float], list[float]]]:
-    self.arrange_min_max(0)
-    for i in range(1, self.num_gen+1):
-      self.pop.next_generation()
-      self.arrange_min_max(i)
-    assert self.max_sol is not None
-    assert self.min_sol is not None
+  def __arrange_min_max(self, i: int) -> None:
+    _max_sol, _min_sol, _max, _avg, _min=self.__pop.get_max_avg_min()
+    if _max>self.__max_of_max:
+      self.__max_of_max=_max
+      self.__max_sol=_max_sol
+    if _min<self.__min_of_min:
+      self.__min_of_min=_min
+      self.__min_sol=_min_sol
+    self.__maxs[i]=_max
+    self.__avgs[i]=_avg
+    self.__mins[i]=_min
+
+  @property
+  def curr_generations(self) -> int:
+    return self.__curr_generations
+
+  def get_statistics(self) -> tuple[tuple[util.IndividualType, util.IndividualType], tuple[float, float], tuple[list[float], list[float], list[float]]]:
+    assert self.__max_sol is not None
+    assert self.__min_sol is not None
     return (
-      (self.max_sol, self.min_sol),
-      (self.max_of_max, self.min_of_min),
-      (self.maxs, self.avgs, self.mins),
+      (self.__max_sol, self.__min_sol),
+      (self.__max_of_max, self.__min_of_min),
+      (self.__maxs[:self.__curr_generations+1], self.__avgs[:self.__curr_generations+1], self.__mins[:self.__curr_generations+1]),
     )
-
-  def arrange_min_max(self, i: int) -> None:
-    _max_sol, _min_sol, _max, _avg, _min=self.pop.get_max_avg_min()
-    if _max>self.max_of_max:
-      self.max_of_max=_max
-      self.max_sol=_max_sol
-    if _min<self.min_of_min:
-      self.min_of_min=_min
-      self.min_sol=_min_sol
-    self.maxs[i]=_max
-    self.avgs[i]=_avg
-    self.mins[i]=_min
