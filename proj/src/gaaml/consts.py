6fb129e1c31067a62c9a2196e899ada54dffd2bd
Zlog <zlog@protonmail.com>
2026-04-01 16:09:00 +0200

    change in theory and in constants to represent that

--- proj/src/gaaml/consts.py
+++ proj/src/gaaml/consts.py
@@ -8,8 +8,6 @@ BIN_PART_LIST_LEN=('layer_num', (7, 1, 128))
 BIN_PART_NEURON_NUM_SEED=('neuron_num_seed', (10, 0, 1023))
 BIN_PART_NEURON_TYPE_SEED=('neuron_type_seed', (10, 0, 1023))
 BIN_PART_REST=(
-  # ('neuron_num_seed', (10, 0, 1023)),
-  # ('neuron_type_seed', (10, 0, 1023)),
   ('optimizer', (1, 0, 1)),
   ('learning_rate', (12, 1, 4096)),
   # TODO: maybe this version [2^(-13) - 0.5] → [2^(-13) - 1.]
@@ -18,5 +16,5 @@ BIN_PART_REST=(
 )
 
 # (num_of_elem_bits, min_v, max_v)
-NEURON_NUM=(20, 2, 1048577)
+NEURON_NUM=(10, 2, 724)
 NEURON_TYPE=(2, 0, 3)
