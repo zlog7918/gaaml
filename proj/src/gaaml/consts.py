5a343540543063d45a6d4fca29e968871b666b1b
Zlog <zlog@protonmail.com>
2026-05-03 15:46:36 +0200

    change of maximal specification for num of hidden layers and epochs

--- proj/src/gaaml/consts.py
+++ proj/src/gaaml/consts.py
@@ -9,7 +9,7 @@ BIN_PART_BATCH_NAME='batch_size'
 BIN_PART_EPOCHS_NAME='epochs'
 
 # (name, (num_of_bits, min_v, max_v))
-BIN_PART_LIST_LEN=('layer_num', (7, 1, 128))
+BIN_PART_LIST_LEN=('layer_num', (5, 1, 32))
 BIN_PART_NEURON_NUM_SEED=('neuron_num_seed', (10, 0, 1023))
 BIN_PART_NEURON_TYPE_SEED=('neuron_type_seed', (10, 0, 1023))
 BIN_PART_REST=(
@@ -17,8 +17,8 @@ BIN_PART_REST=(
   (BIN_PART_LEARNING_RATE_NAME, (12, 1, 4096)),
   # TODO: maybe this version [2^(-13) - 0.5] → [2^(-13) - 1.]
   # (BIN_PART_LEARNING_RATE_NAME, (13, 1, 8192)),
+  (BIN_PART_EPOCHS_NAME, (7, 1, 128)),
   (BIN_PART_BATCH_NAME, (10, 1, 1024)),
-  (BIN_PART_EPOCHS_NAME, (10, 1, 1024)),
 )
 
 # (num_of_elem_bits, min_v, max_v)
