515b1fb16be31e1293114605bab3515932e58212
Zlog <zlog@protonmail.com>
2026-07-15 10:44:15 +0200

    expose some more content

--- proj/src/gaaml/__init__.py
+++ proj/src/gaaml/__init__.py
@@ -2,5 +2,15 @@ import os
 if os.environ.get('KERAS_BACKEND') is None:
   os.environ['KERAS_BACKEND']='torch'
 from .core import cr_network
+from . import consts as const
+from .utils import cr_net_from_ind
+from .classes.Generations import Generations
+from .classes.NetIndividual import NetIndividual
 
-__all__=["cr_network"]
+__all__=[
+  'const',
+  'cr_network',
+  'Generations',
+  'NetIndividual',
+  'cr_net_from_ind',
+]
